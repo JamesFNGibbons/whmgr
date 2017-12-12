eb66cee6b9fe585a2bfecede7a971044c17cdc8f
James Gibbons <jamesfngibbons@icloud.com>
2017-12-12 14:27:53 +0000

    Addec clientarea add db

--- admin/routes/clientarea-databases.js
+++ admin/routes/clientarea-databases.js
@@ -1,4 +1,50 @@
 const router = require('express').Router();
+const mysql = require('mysql');
+
+/**
+  * Route used to add a new database to
+	* the clients account.
+*/
+router.post('/add', (req, res) => {
+	if(req.session.client_loggedin){
+		let account = req.session.client_username;
+		let username = `${account}_du_${req.body.user}`;
+		let database = `${account}_db_${req.body.name}`;
+		let password = req.body.password;
+
+		// Insert the details into the database.
+		req.db.collection('databases').insert({
+			account: account,
+			username: username,
+			password: password,
+			database: database
+		}, (err) => {
+			if(err) throw err;
+		});
+
+		// Create the database, and user with mysql.
+		let connection = mysql.createConnection({
+			host: "localhost",
+			user: "root",
+			password: "my_password"
+		});
+		connection.connect();
+		connection.query(`CREATE DATABASE IF NOT EXISTS ${database}`, (err) => {
+			if(err) throw err;
+		});
+		connection.query(`CREATE USER '${username}'@'localhost' IDENTIFIED BY '${password}'`, (err) => {
+			if(err) throw err;
+		});
+		connection.query(`GRANT ALL PRIVILEGES ON ${database} . * TO '${username}'@'localhost';`, (err) => {
+			if(err) throw err;
+		});
+
+		res.redirect('/clientarea/databases');
+	}
+	else{
+		res.redirect('/');
+	}
+});
 
 /**
   * Route used to display the create database page.
@@ -12,6 +58,44 @@ router.get('/add', (req, res) => {
 	}
 	else{
 		res.redirect('/');
+	}
+});
+
+/**
+  * Route used to login to phpmyadmin, VIA GET.
+*/
+router.get('/do-phpmyadmin', (req, res) => {
+	if(req.session.client_loggedin){
+		let database = req.query.database
+		if(database){
+			req.db.collection('databases').find({
+				database: database
+			}).toArray((err, docs) => {
+				if(err) throw err;
+				else{
+					if(docs.length > 0){
+						req.db.collection('settings').find({
+							name: 'url'
+						}).toArray((err, setting) => {
+							if(err) throw err;
+							else{
+								let url = setting[0].value;
+								let username = docs[0].username;
+								let password = docs[0].password;
+								let pma_connection = `http://${url}/phpmyadmin/index.php?lang=en&server=1&pma_username=${username}&pma_password=${password}`;
+								res.redirect(pma_connection);
+							}
+						});
+					}
+					else{
+						res.end('Invalid database posted.');
+					}
+				}
+			});
+		}
+	}
+	else{
+		res.end('Authentication error.');
 	}
 });
 
@@ -50,6 +134,28 @@ router.post('/do-phpmyadmin', (req, res) => {
 	}
 	else{
 		res.redirect('/clientarea');
+	}
+});
+
+/**
+  * Route used to list the clients databases.
+*/
+router.get('/', (req, res) => {
+	if(req.session.client_loggedin){
+		req.db.collection('databases').find({
+			account: req.session.client_username
+		}).toArray((err, databases) => {
+			if(err) throw err;
+			else{
+				res.render('databases/clientarea-list', {
+					title: "Databases",
+					databases: databases
+				});
+			}
+		});
+	}
+	else{
+		res.redirect('/');
 	}
 });
 
